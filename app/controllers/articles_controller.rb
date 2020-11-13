f23b34937f3917cd169f7747739307af492ef0d4
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2020-11-13 17:33:08 +0200

    finished blog comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,10 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
-  end  
+    @comment = Comment.new
+    @comment.article_id = @article.id
+    
+  end
 
   def new
     @article = Article.new
